27264ec61e777dc50355984b55759de00515f682
Михаил Юрчук <71945861+w0resT@users.noreply.github.com>
2025-09-26 15:19:21 +0300

    Initial Commit

---
+++ RickAndMortyExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyExplorer
+//
+//  Created by Михаил Юрчук on 26.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
